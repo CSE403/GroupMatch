3af4952de97a99d7593226980017949e7a39bece
Justin Ryll <jtryll@gmail.com>
2012-05-02 17:59:26 -0700

    css verification

--- Application/Themes/Default/Controllers/Main/Poll/solution.php
+++ Application/Themes/Default/Controllers/Main/Poll/solution.php
@@ -1,1 +1,14 @@
-poll solution
+<section>
+	<header>
+		<button class="green">Back to Poll</button>
+		<h1>Poll title here</h1>
+	</header>
+	<section id="report" class="indent">
+		<div id="happy_meter">
+			<h1>Happiness</h1>
+			<div id="numeric_value">55%</div>
+			<div id="meter_background"><div id="meter_value"></div></div>
+		</div>
+		<div id="answer">Answer Here</div>
+	</section>
+</section>
