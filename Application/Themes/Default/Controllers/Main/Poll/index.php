574f1df04d84dec8abd3cfd07cdbd63a0b95d89f
theDeus <vakudva@gmail.com>
2012-05-02 18:51:02 -0700

    Update Application/Themes/Default/Controllers/Main/Poll/index.php

--- Application/Themes/Default/Controllers/Main/Poll/index.php
+++ Application/Themes/Default/Controllers/Main/Poll/index.php
@@ -1,7 +1,13 @@
+<!-- 
+    Poll/index.php
+    
+    This php webpage is for the actual poll. On this page, users can see who has already participated in the poll, 
+    and those participant's responses as well as their own. If the poll is not closed, users can also add their response
+    to the poll.
+-->
 <?php
     $content = $this->registry->config->siteUrl.$this->getThemeLocation()."Images/";
 ?>
-
 <section>
     <header>
         <button class="green">View Solution</button>
@@ -12,6 +18,10 @@
             <tr><th></th><th>Tuesday 4pm</th><th>Thursday 5pm</th><th>Saturday 1pm</th></tr>
         </thead>
         <tbody>
+        <!-- 
+            Each participants answers will be stored here. On the page, it will be depicted as a row of (in this case)
+            stars depicting how much the user want that option.
+        -->
             <tr class="alt">
                 <td class="participant">Justin Ryll</td>
                 <td><img src="<?= $content?>star_small.png" width="20" height="20"/><img src="<?= $content?>star_empty_small.png" width="20" height="20"/><img src="<?= $content?>star_empty_small.png" width="20" height="20"/></td>
