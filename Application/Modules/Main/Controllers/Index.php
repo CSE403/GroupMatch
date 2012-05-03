44d90d9839a5841ae3e84bdc70bfc3aaff7e434a
theDeus <vakudva@gmail.com>
2012-05-03 07:25:49 -0700

    Update Application/Modules/Main/Controllers/Index.php

--- Application/Modules/Main/Controllers/Index.php
+++ Application/Modules/Main/Controllers/Index.php
@@ -1,16 +1,32 @@
 <?php
 namespace Application\Modules\Main\Controllers;
-
+/**
+	The Index controller houses all functionality needed on the website's frontpage, including redirecting
+	to the user's homepage if he/she is logged in, or redirecting him to the registration page if the user
+	wants to register.
+*/
 class Index extends \Saros\Application\Controller
 {
+	/**
+		Initializes the page. 
+		@ZFR: Does nothing.
+	*/
     public function init() {
        
     }
     
-	public function indexAction()
-	{                                                        
-	}
+    /**
+    	@ZFR: Placeholder function for any required logic needed for the user on the front page. 
+    	Currently does nothing.
+    */
+    public function indexAction()
+    {      
+    	
+    }
     
+    /**
+    	Redirects the user to the registration page if the user clicks on the "Register" button.
+    */
     public function registerAction()
     {
         $this->view->headStyles()->addStyle("register");
