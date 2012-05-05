f655cc2fd354583b7d34fc2503b618f7a614bb98
Justin Ryll <jtryll@gmail.com>
2012-05-05 12:29:55 -0700

    corrected create poll, and polls html

--- Application/Themes/Default/Controllers/Main/Account/create.php
+++ Application/Themes/Default/Controllers/Main/Account/create.php
@@ -6,62 +6,74 @@
     to choose from, as well as other details like specific option group limit.
 -->
 <section>
-    <header>
-        <button class="green">Create</button>
-        <input name="title" type="text" placeholder="Tile or Question"/>
-    </header>
-    <form name="create_poll" class="indent">
-        <section>
-            <!--
+	<header>
+		<div><h1>Title</h1></div>
+		<button form="create_poll" type="submit" class="green">Create</button>
+		<input form="create_poll" name="title" type="text" placeholder="Tile or Question"/>
+	</header>
+	<section class="form_error">
+		This is an error message
+	</section>
+	<form name="create_poll" class="indent">
+		<section>
+			<h1>Description</h1>
+			<footer>
+				<textarea rows="3" cols="50" maxlength="300" placeholder="Type a description here"></textarea>
+			</footer>
+		</section>
+		<section>
+			<!--
                 Specify overall how many people can use the poll before it is closed,
                 based on the amount of answers you give them.
             -->
-            <h1>Universal Participant Limit</h1>
-            <footer>
-                <input name="option_all_limit" type="checkbox"/> Participant limit per answer <input name="option_all_limit_amount" type="number" placeholder="10" disabled="disabled">
-            </footer>
-        </section>
-        <section>
-            <!--Specify how users of the poll will give his or her answers to your options.-->
-            <h1>Answer type</h1>
-            <footer>
-                <div><input name="answer_type" type="radio" checked="checked"/> Yes / No</div>
-                <div><input name="answer_type" type="radio"/> Yes / Neutral / No</div>
-                <div><input name="answer_type" type="radio"/> Uniquely rate each option</div>
-                <div><input name="answer_type" type="radio"/> Rate on scale between 1 and <input name="scale_max" type="number" placeholder="4" min="5" max="100" value="4" disabled="disabled"></div>
-            </footer>
-        </section>
-        <section>
-            <!-- The options that the user will create will be displayed here.-->
-            <h1>Options / Unique Participant Limit</h1>
-            <footer>
-                <ul>
-                    <li>
-                        <input name="option_1" type="text" placeholder="Option"/>
-                        <input name="option_1_limit" type="checkbox"/>
-                        <input name="option_1_limit_amount" type="number" placeholder="10" min="1" disabled="disabled">
-                    </li>
-                    <li>
-                        <input name="option_1" type="text" placeholder="Option"/>
-                        <input name="option_1_limit" type="checkbox"/>
-                        <input name="option_1_limit_amount" type="number" placeholder="10" min="1" disabled="disabled">
-                    </li>
-                    <li>
-                        <input name="option_1" type="text" placeholder="Option"/>
-                        <input name="option_1_limit" type="checkbox"/>
-                        <input name="option_1_limit_amount" type="number" placeholder="10" min="1" disabled="disabled">
-                    </li>
-                    <li>
-                        <input name="option_1" type="text" placeholder="Option"/>
-                        <input name="option_1_limit" type="checkbox"/>
-                        <input name="option_1_limit_amount" type="number" placeholder="10" min="1" disabled="disabled">
-                    </li>
-                    <li>
-                        
-                    </li>
-                </ul>
-                <button class="green">Add Answers</button><!--This button allows the user to add additional options to the poll-->
-            </footer>
-        </section>
-    </form>
+			<h1>Universal Participant Limit</h1>
+			<footer>
+				<input name="option_all_limit" type="checkbox" /> Participant limit per answer <input name="option_all_limit_amount" type="number" placeholder="10" min="1" disabled="disabled">
+			</footer>
+		</section>
+		<section>
+			<!--Specify how users of the poll will give his or her answers to your options.-->
+			<h1>Answer type</h1>
+			<footer>
+				<div><input name="answer_type" type="radio" value="1" checked="checked" /> Yes / No</div>
+				<div><input name="answer_type" type="radio" value="2" /> Yes / Neutral / No</div>
+				<div><input name="answer_type" type="radio" value="3" /> Uniquely rate each option</div>
+				<div><input name="answer_type" type="radio" value="4" /> Rate on scale between 1 and <input name="scale_max" type="number" placeholder="4" min="4" max="100" value="4" disabled="disabled" /></div>
+			</footer>
+		</section>
+		<section>
+			<!-- The options that the user will create will be displayed here.-->
+			<h1>Options / Unique Participant Limit</h1>
+			<footer>
+				<ul id="option_list">
+					<li>
+						<input name="option_1" type="text" placeholder="Option" />
+						<input name="option_limit_1" type="checkbox" />
+						<input name="option_limit_amount_1" type="number" placeholder="10" min="1" disabled="disabled" />
+					</li>
+					<li>
+						<input name="option_2" type="text" placeholder="Option" />
+						<input name="option_limit_2" type="checkbox" />
+						<input name="option_limit_amount_2" type="number" placeholder="10" min="1" disabled="disabled" />
+					</li>
+					<li>
+						<input name="option_3" type="text" placeholder="Option" />
+						<input name="option_limit_3" type="checkbox" />
+						<input name="option_limit_amount_3" type="number" placeholder="10" min="1" disabled="disabled" />
+					</li>
+					<li>
+						<input name="option_4" type="text" placeholder="Option" />
+						<input name="option_limit_4" type="checkbox" />
+						<input name="option_limit_amount_4" type="number" placeholder="10" min="1" disabled="disabled" />
+					</li>
+					<li>
+						<input name="option_5" type="text" placeholder="Option" />
+						<input name="option_limit_5" type="checkbox" />
+						<input name="option_limit_amount_5" type="number" placeholder="10" min="1" disabled="disabled" />
+					</li>
+				</ul>
+				<button name="add_more_options" class="green">Add Answers</button><!--This button allows the user to add additional options to the poll-->
+			</footer>
+		</section>
+	</form>
 </section>
--- Application/Themes/Default/Controllers/Main/Account/index.php
+++ Application/Themes/Default/Controllers/Main/Account/index.php
@@ -8,32 +8,36 @@
     $pollPage = $GLOBALS["registry"]->utils->makeLink("Poll");
 ?>
 <section>
-    <header>
-        <h1>Poll Management</h1>
-    </header>
-    <!-- 
+	<header>
+		<h1>Poll Management</h1>
+	</header>
+	<!-- 
         All polls that the user has created will be displayed here. Each poll item on the user's page
         gives the name of that poll and a link to the actual webpage of that poll, so the user can see
         who has used his poll, and to see the current solution.
     -->
-    <ul class="indent">
-        <li>
-            <button class="red">Delete</button>
-            <header><a href="<?= $pollPage ?>">When should our team meet?</a></header>
-            <footer>www.groupmatch.com/poll/ogn3f0waf3lqvnlk230</footer>
-        </li>
-        <li>
-            <button class="red">Delete</button>
-            <header><a href="<?= $pollPage ?>">Group project preference</a></header>
-            <footer>www.groupmatch.com/poll/sw3jkdvb3ek9uthj5</footer>
-        </li>
-        <li>
-            <button class="red">Delete</button>
-            <header><a href="<?= $pollPage ?>">Volunteer at the homeless shelter</a></header>
-            <footer>www.groupmatch.com/poll/asw3v03u1u151208ndv</footer>
-        </li>
-    </ul>
+	<section class="indent">
+		<h2 id="no_polls_message" style="display:none;">You don't have any polls</h2>
+		<ul id="poll_list">
+			<li>
+				<button class="red" name="delete_poll">Delete</button>
+				<header><a href="<?= $pollPage ?>">When should our team meet?</a></header>
+				<footer>www.groupmatch.com/poll/ogn3f0waf3lqvnlk230</footer>
+			</li>
+			<li>
+				<button class="red" name="delete_poll">Delete</button>
+				<header><a href="<?= $pollPage ?>">Group project preference</a></header>
+				<footer>www.groupmatch.com/poll/sw3jkdvb3ek9uthj5</footer>
+			</li>
+			<li>
+				<button class="red" name="delete_poll">Delete</button>
+				<header><a href="<?= $pollPage ?>">Volunteer at the homeless shelter</a></header>
+				<footer>www.groupmatch.com/poll/asw3v03u1u151208ndv</footer>
+			</li>
+		</ul>
+	</section>
 </section>
+
 <section>
     <header>
         <!-- Link to creating new polls-->
